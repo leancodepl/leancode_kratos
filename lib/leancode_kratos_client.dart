d4dcfc21c53dde877f18c1af79cfef53d1f4b8a7
Denis Łaskawiec <denis.laskawiec@leancode.pl>
2023-07-20 11:50:10 +0200

    export register domain model

--- lib/leancode_kratos_client.dart
+++ lib/leancode_kratos_client.dart
@@ -3,6 +3,7 @@ library leancode_kratos_client;
 export 'src/kratos_client.dart';
 export 'src/login/domain/login_response.dart';
 export 'src/logout/logout.dart';
+export 'src/registration/domain/registration_domain.dart';
 export 'src/registration/domain/registration_response.dart';
 export 'src/utils/credentials_storage.dart';
 export 'src/verification/domain/verification_flow_domain.dart';
