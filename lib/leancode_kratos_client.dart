49f2f1b3974f1d0a4109a351045447add2531098
Denis Łaskawiec <denis.laskawiec@leancode.pl>
2023-08-16 17:32:55 +0200

    export kratos_errors file

--- lib/leancode_kratos_client.dart
+++ lib/leancode_kratos_client.dart
@@ -6,5 +6,6 @@ export 'src/logout/logout.dart';
 export 'src/registration/domain/registration_domain.dart';
 export 'src/registration/domain/registration_response.dart';
 export 'src/utils/credentials_storage.dart';
+export 'src/utils/kratos_errors.dart';
 export 'src/verification/domain/verification_flow_domain.dart';
 export 'src/verification/domain/verification_result.dart';
