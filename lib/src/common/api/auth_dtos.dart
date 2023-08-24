21258f6481ef8b197e58dda697b6b97cb8c0ac12
Robert Odrowaz <robert@odrowaz.dev>
2023-08-24 10:39:44 +0200

    Extend methods for social login

---
+++ lib/src/common/api/auth_dtos.dart
@@ -0,0 +1,140 @@
+import 'dart:convert';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_dtos.freezed.dart';
+part 'auth_dtos.g.dart';
+
+AuthFlowDto authFlowDtoFromJson(String str) =>
+    AuthFlowDto.fromJson(json.decode(str) as Map<String, dynamic>);
+
+@freezed
+class AuthFlowDto with _$AuthFlowDto {
+  const factory AuthFlowDto({
+    required String id,
+    String? oauth2LoginChallenge,
+    required String type,
+    required DateTime expiresAt,
+    required DateTime issuedAt,
+    required String requestUrl,
+    String? returnTo,
+    required UiDto ui,
+    String? sessionTokenExchangeCode,
+  }) = _AuthFlowDto;
+
+  factory AuthFlowDto.fromJson(Map<String, dynamic> json) =>
+      _$AuthFlowDtoFromJson(json);
+}
+
+@freezed
+class UiDto with _$UiDto {
+  const factory UiDto({
+    required String action,
+    required String method,
+    required List<NodeDto> nodes,
+    List<MessageDto>? messages,
+  }) = _UiDto;
+
+  const UiDto._();
+
+  factory UiDto.fromJson(Map<String, dynamic> json) => _$UiDtoFromJson(json);
+
+  String? get loginMethod {
+    return nodes
+        .map(
+          (node) => switch (node.attributes) {
+            AttributesDto(
+              name: 'method',
+              value: final String attributeValue,
+            ) =>
+              attributeValue,
+            _ => null,
+          },
+        )
+        .firstOrNull;
+  }
+}
+
+@freezed
+class NodeDto with _$NodeDto {
+  const factory NodeDto({
+    required String type,
+    required String group,
+    required AttributesDto attributes,
+    required List<MessageDto> messages,
+    required MetaDto meta,
+  }) = _NodeDto;
+
+  factory NodeDto.fromJson(Map<String, dynamic> json) =>
+      _$NodeDtoFromJson(json);
+}
+
+@freezed
+class AttributesDto with _$AttributesDto {
+  const factory AttributesDto({
+    String? name,
+    String? type,
+    dynamic value,
+    bool? required,
+    bool? disabled,
+    String? nodeType,
+    String? autocomplete,
+  }) = _AttributesDto;
+
+  factory AttributesDto.fromJson(Map<String, dynamic> json) =>
+      _$AttributesDtoFromJson(json);
+}
+
+@freezed
+class MessageDto with _$MessageDto {
+  const factory MessageDto({
+    required int id,
+    required String text,
+    required String type,
+    MessageContextDto? context,
+  }) = _MessageDto;
+
+  factory MessageDto.fromJson(Map<String, dynamic> json) =>
+      _$MessageDtoFromJson(json);
+}
+
+@freezed
+class MessageContextDto with _$MessageContextDto {
+  const factory MessageContextDto({
+    String? reason,
+  }) = _MessageContextDto;
+
+  factory MessageContextDto.fromJson(Map<String, dynamic> json) =>
+      _$MessageContextDtoFromJson(json);
+}
+
+@freezed
+class MetaDto with _$MetaDto {
+  const factory MetaDto({
+    LabelDto? label,
+  }) = _MetaDto;
+
+  factory MetaDto.fromJson(Map<String, dynamic> json) =>
+      _$MetaDtoFromJson(json);
+}
+
+@freezed
+class LabelDto with _$LabelDto {
+  const factory LabelDto({
+    required int id,
+    required String text,
+    required String type,
+    ContextDto? context,
+  }) = _LabelDto;
+
+  factory LabelDto.fromJson(Map<String, dynamic> json) =>
+      _$LabelDtoFromJson(json);
+}
+
+@freezed
+class ContextDto with _$ContextDto {
+  const factory ContextDto() = _ContextDto;
+
+  factory ContextDto.fromJson(Map<String, dynamic> json) =>
+      _$ContextDtoFromJson(json);
+}
--- test/fixtures/get_registration_flow_fixture.dart
+++ test/fixtures/get_registration_flow_fixture.dart
@@ -1,146 +1,190 @@
 const registrationFlowResponse = '''
 {
-    "id": "9cff2675-a983-4b36-9957-ad722287915e",
-    "oauth2_login_challenge": null,
-    "type": "api",
-    "expires_at": "2023-07-12T17:24:21.123581527Z",
-    "issued_at": "2023-07-12T16:24:21.123581527Z",
-    "request_url": "https://auth.leancode-internal-kratos.test.lncd.pl/self-service/registration/api",
-    "ui": {
-        "action": "https://auth.leancode-internal-kratos.test.lncd.pl/self-service/registration?flow=9cff2675-a983-4b36-9957-ad722287915e",
-        "method": "POST",
-        "nodes": [
-            {
-                "type": "input",
-                "group": "default",
-                "attributes": {
-                    "name": "csrf_token",
-                    "type": "hidden",
-                    "value": "",
-                    "required": true,
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {}
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "traits.email",
-                    "type": "email",
-                    "required": true,
-                    "autocomplete": "email",
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1070002,
-                        "text": "E-Mail",
-                        "type": "info"
-                    }
-                }
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "password",
-                    "type": "password",
-                    "required": true,
-                    "autocomplete": "new-password",
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1070001,
-                        "text": "Password",
-                        "type": "info"
-                    }
-                }
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "traits.given_name",
-                    "type": "text",
-                    "required": true,
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1070002,
-                        "text": "Given name",
-                        "type": "info"
-                    }
-                }
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "traits.family_name",
-                    "type": "text",
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1070002,
-                        "text": "Family name",
-                        "type": "info"
-                    }
-                }
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "traits.regulations_accepted",
-                    "type": "checkbox",
-                    "required": true,
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1070002,
-                        "text": "I accept Terms of Service and Privacy Policy",
-                        "type": "info"
-                    }
-                }
-            },
-            {
-                "type": "input",
-                "group": "password",
-                "attributes": {
-                    "name": "method",
-                    "type": "submit",
-                    "value": "password",
-                    "disabled": false,
-                    "node_type": "input"
-                },
-                "messages": [],
-                "meta": {
-                    "label": {
-                        "id": 1040001,
-                        "text": "Sign up",
-                        "type": "info",
-                        "context": {}
-                    }
-                }
-            }
-        ]
-    }
+  "id": "213e3ea0-6b7d-472a-b9bd-0d957716ff14",
+  "type": "api",
+  "expires_at": "2023-08-24T12:44:50.614846417Z",
+  "issued_at": "2023-08-24T11:44:50.614846417Z",
+  "request_url": "https://auth.exampleapp.test.lncd.pl/self-service/registration/api?return_session_token_exchange_code=true\u0026return_to=pl.leancode.template.tst%3A%2F%2Fapp",
+  "return_to": "pl.leancode.template.tst://app",
+  "ui": {
+    "action": "https://auth.exampleapp.test.lncd.pl/self-service/registration?flow=213e3ea0-6b7d-472a-b9bd-0d957716ff14",
+    "method": "POST",
+    "nodes": [
+      {
+        "type": "input",
+        "group": "default",
+        "attributes": {
+          "name": "csrf_token",
+          "type": "hidden",
+          "value": "",
+          "required": true,
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {}
+      },
+      {
+        "type": "input",
+        "group": "oidc",
+        "attributes": {
+          "name": "provider",
+          "type": "submit",
+          "value": "apple",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": {
+            "id": 1040002,
+            "text": "Sign up with Apple",
+            "type": "info",
+            "context": { "provider": "Apple" }
+          }
+        }
+      },
+      {
+        "type": "input",
+        "group": "oidc",
+        "attributes": {
+          "name": "provider",
+          "type": "submit",
+          "value": "facebook",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": {
+            "id": 1040002,
+            "text": "Sign up with Facebook",
+            "type": "info",
+            "context": { "provider": "Facebook" }
+          }
+        }
+      },
+      {
+        "type": "input",
+        "group": "oidc",
+        "attributes": {
+          "name": "provider",
+          "type": "submit",
+          "value": "google",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": {
+            "id": 1040002,
+            "text": "Sign up with Google",
+            "type": "info",
+            "context": { "provider": "Google" }
+          }
+        }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "traits.email",
+          "type": "email",
+          "required": true,
+          "autocomplete": "email",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": { "label": { "id": 1070002, "text": "E-Mail", "type": "info" } }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "password",
+          "type": "password",
+          "required": true,
+          "autocomplete": "new-password",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": { "id": 1070001, "text": "Password", "type": "info" }
+        }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "traits.given_name",
+          "type": "text",
+          "required": true,
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": { "id": 1070002, "text": "Given name", "type": "info" }
+        }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "traits.family_name",
+          "type": "text",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": { "id": 1070002, "text": "Family name", "type": "info" }
+        }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "traits.regulations_accepted",
+          "type": "checkbox",
+          "required": true,
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": {
+            "id": 1070002,
+            "text": "I accept Terms of Service and Privacy Policy",
+            "type": "info"
+          }
+        }
+      },
+      {
+        "type": "input",
+        "group": "password",
+        "attributes": {
+          "name": "method",
+          "type": "submit",
+          "value": "password",
+          "disabled": false,
+          "node_type": "input"
+        },
+        "messages": [],
+        "meta": {
+          "label": {
+            "id": 1040001,
+            "text": "Sign up",
+            "type": "info",
+            "context": {}
+          }
+        }
+      }
+    ]
+  },
+  "session_token_exchange_code": "A739SatzLgiz9Tu3AD2VRcM5UKk7OMPcdd8tFxVo5MINa5V3xkcnFDJK4oJB3s0C"
 }
+
 ''';
