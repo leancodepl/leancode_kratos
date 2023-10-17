1add8292a5b58edfc822d0977780adcb003061de
Denis Łaskawiec <denis.laskawiec@leancode.pl>
2023-10-17 14:53:10 +0200

    Add userId to profile model

--- lib/src/profile/domain/profile.dart
+++ lib/src/profile/domain/profile.dart
@@ -5,8 +5,9 @@ sealed class Profile {}
 class ErrorGettingProfile extends Profile {}
 
 class ProfileData extends Profile {
-  ProfileData({required this.flowId, required this.traits});
+  ProfileData({required this.flowId, required this.traits, required this.userId});
 
   final String flowId;
   final List<ProfileTrait> traits;
+  final String userId;
 }
